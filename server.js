992598bc4c8480972a1bdcb9936ff310d7a4946d
Malvino30-06 <malvinogarcia2@gmail.com>
2026-03-28 15:44:42 -0300

    feat: adicionar e testar rotas, além de determinar o que será implementado

--- server.js
+++ server.js
@@ -1,9 +1,28 @@
 import express from "express";
 
 const app = express();
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("certo");
+const produtos = [];
+
+app.post("/produtos", (req, res) => {
+  produtos.push(req.body);
+  res.status(201).json(req.body);
+});
+
+app.get("/produtos", (req, res) => {
+  res.status(200).json(produtos);
 });
 
 app.listen(3000);
+
+/*
+
+API 
+
+- criar produto
+- listar produtos
+- atualizar produto
+- deletar produto
+
+*/
