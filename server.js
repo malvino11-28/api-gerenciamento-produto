ff9ebc38c61660f4cba67236c4ae258aa3a5f53f
Malvino30-06 <malvinogarcia2@gmail.com>
2026-03-28 16:37:49 -0300

    feat: implementar CRUD para produtos com rotas de criação, atualização, listagem e delete

--- server.js
+++ server.js
@@ -3,19 +3,19 @@ import express from "express";
 const app = express();
 app.use(express.json());
 
-c;
+let id = 1;
 
-const produtos = [
-  {
-    nome: "",
-    descricao: "",
-    valor: 0,
-    quantidade: 0,
-  },
-];
+const produtos = [];
 
 app.post("/produtos", (req, res) => {
-  const novoProd = req.body;
+  const { nome, descricao, valor, quantidade } = req.body;
+  const novoProd = {
+    id: id++,
+    nome,
+    descricao,
+    valor,
+    quantidade,
+  };
 
   produtos.push(novoProd);
   res.status(201).json(novoProd);
@@ -23,6 +23,31 @@ app.post("/produtos", (req, res) => {
 
 app.get("/produtos", (req, res) => {
   res.status(200).json(produtos);
+});
+
+app.put(`/produtos/:${id}`, (req, res) => {
+  const { id } = req.params;
+  const { nome, descricao, valor, quantidade } = req.body;
+
+  const busca = produtos.findIndex((p) => p.id === id);
+
+  produtos[busca] = {
+    id: id,
+    nome: nome || produtos[busca].nome,
+    descricao: descricao || produtos[busca].descricao,
+    valor: valor || produtos[busca].valor,
+    quantidade: quantidade || produtos[busca].quantidade,
+  };
+
+  res.status(200).json(produtos[busca]);
+});
+
+app.delete(`/produtos/${id}`, (req, res) => {
+  const { id } = req.params;
+  const busca = produtos.findIndex((p) => p.id === id);
+  produtos.splice(busca, 1);
+
+  res.status(204).send();
 });
 
 app.listen(3000);
