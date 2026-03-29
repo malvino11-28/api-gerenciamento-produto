d0617c9c663225f5b0f1e9ed32f07d3518a3b4a9
Malvino30-06 <malvinogarcia2@gmail.com>
2026-03-29 18:30:13 -0300

    fix: alterando conexão com o banco de dados e melhorar tratamento de respostas nas rotas para funcionamento do deploy

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ dotenv.config();
 import express from "express";
 import mysql from "mysql2/promise";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 app.use(cors());
@@ -10,23 +12,28 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 let id = 1;
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
+let pool;
 
-try {
-  await pool.execute("SELECT 1");
-  console.log("✅ Conectado ao MySQL!");
-} catch (error) {
-  console.error("❌ Erro ao conectar no MySQL:", error);
+if (process.env.USE_DB === "true") {
+  pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  });
+
+  try {
+    await pool.execute("SELECT 1");
+    console.log("✅ Conectado ao MySQL!");
+  } catch (error) {
+    console.error("❌ Erro ao conectar no MySQL:", error);
+  }
 }
 
 const banco = process.env.USE_DB === "true";
 const produtos = [];
-const path = require("path");
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
@@ -66,7 +73,7 @@ app.post("/produtos", async (req, res) => {
         quantidade,
       };
       produtos.push(novoProd);
-      res.status(201).json(novoProd);
+      return res.status(201).json(novoProd);
     }
     const [result] = await pool.execute(
       "INSERT INTO produtos (nome, descricao, valor, quantidade) VALUES (?, ?, ?, ?)",
@@ -121,7 +128,7 @@ app.put("/produtos/:id", async (req, res) => {
         quantidade: novaQTD,
       };
 
-      res.status(200).json(produtos[busca]);
+      return res.status(200).json(produtos[busca]);
     }
     const [result] = await pool.execute(
       "UPDATE produtos SET nome = ?, descricao = ?, valor = ?, quantidade = ? WHERE id = ?",
@@ -132,7 +139,7 @@ app.put("/produtos/:id", async (req, res) => {
       return res.status(404).json({ erro: "Produto não encontrado. " });
     }
     return res.status(200).json({
-      if: idParam,
+      id: idParam,
       nome,
       descricao,
       valor: Number(valor),
@@ -160,7 +167,7 @@ app.delete("/produtos/:id", async (req, res) => {
       }
       produtos.splice(busca, 1);
 
-      res.status(204).send();
+      return res.status(204).send();
     }
     const [result] = await pool.execute("DELETE FROM produtos WHERE id = ?", [
       idParam,
