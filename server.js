ca70dfb804ffc44b0e659bd93856b6e924bd5ecd
Malvino <malvinogarcia2@gmail.com>
2026-03-29 18:06:01 -0600

    feat: mover arquivos HTML para a pasta pública e ajustar rotas no servidor para o funcionamento do deploy

--- server.js
+++ server.js
@@ -35,10 +35,10 @@ const produtos = [];
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.get("/produtos", async (req, res) => {
